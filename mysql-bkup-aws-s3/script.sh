0e8b046633588d932ba9c194933cdd3a717bde82
Marlon Ruiz <43421777+ruizmarlon@users.noreply.github.com>
2020-06-28 00:45:03 -0400

    Create script.sh

---
+++ mysql-bkup-aws-s3/script.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+
+DB_HOST=$1
+DB_NAME=$2
+DB_PASSWORD=$3
+AWS_KEY=$4
+AWS_SECRET_KEY=$5
+DB_DATE=$(date "+%m%d%y_%N")
+BACKUP=$DB_NAME-$DB_DATE.sql
+AWS_BUCKET=$6
+FILE_PATH=/tmp
+
+echo "Backup beginning"
+echo ""
+echo "Your backup will be named: $BACKUP"
+echo "Target S3 location s3://$AWS_BUCKET"
+echo ""
+mysqldump -u root -p$DB_PASSWORD -h $DB_HOST $DB_NAME > $FILE_PATH/$BACKUP && \
+export AWS_ACCESS_KEY_ID=$AWS_KEY && \
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY && \
+echo "Uploading your backup $BACKUP"
+aws s3 cp $FILE_PATH/$BACKUP s3://$AWS_BUCKET && \
+echo "" && \
+echo "Success!!!"
