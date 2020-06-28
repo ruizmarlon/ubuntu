1ea05fdfd5cd62c385eeec462bf329be92deb766
Marlon Ruiz <43421777+ruizmarlon@users.noreply.github.com>
2020-06-28 01:25:57 -0400

    Update script.sh

--- mysql-bkup-aws-s3/script.sh
+++ mysql-bkup-aws-s3/script.sh
@@ -1,22 +1,23 @@
 #/bin/bash
 
-# set input variables as part of command call (optional)
-# DB_HOST=$1
-# DB_NAME=$2
-# DB_PASSWORD=$3
-# AWS_KEY=$4
-# AWS_SECRET_KEY=$5
-# AWS_BUCKET=$6
+# set input variables as part of command call (optional, use if you don't plan to export first)
+#DB_HOST=$1
+#DB_NAME=$2
+#DB_PASSWORD=$3
+#AWS_KEY=$4
+#AWS_SECRET_KEY=$5
+#AWS_BUCKET=$6
 
 # generate variables locally
 DB_DATE=$(date "+%m%d%y_%N")
 BACKUP=$DB_NAME-$DB_DATE.sql
 FILE_PATH=/tmp
 
-echo "Backup beginning"
+echo ""
+echo "Backup beginning..."
 echo ""
 echo "Your backup will be named: $BACKUP"
-echo "Target S3 location s3://$AWS_BUCKET"
+echo "Target S3 location: s3://$AWS_BUCKET"
 echo ""
 mysqldump -u root -p$DB_PASSWORD -h $DB_HOST $DB_NAME > $FILE_PATH/$BACKUP && \
 export AWS_ACCESS_KEY_ID=$AWS_KEY && \
@@ -27,3 +28,4 @@ echo ""
 aws s3 cp $FILE_PATH/$BACKUP s3://$AWS_BUCKET && \
 echo "" && \
 echo "Success!!!"
+echo ""
